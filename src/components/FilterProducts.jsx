8cdc119ddda61e2ce34cdd2d75783f038f171fbc
Khokhlin Maksym <hohlinmaks@gmail.com>
2026-09-07 13:56:36 +0200

    feat: update filter products

---
+++ src/components/FilterProducts.jsx
@@ -0,0 +1,12 @@
+export default function FilterProducts({selected ,setSelected}){
+    return(
+        <>
+            <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+                <option value="All">All</option>
+                <option value="Clothing">Clothing</option>
+                <option value="Electronics">Electronics</option>
+            </select>
+        </>
+    )
+
+}
